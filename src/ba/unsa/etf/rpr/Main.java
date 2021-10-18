e476a5da6861479b95b134a2a06dd34e6169faf5
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2021-10-18 17:26:52 +0200

    Radi

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,9 +6,27 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
+        System.out.println("Unesite n: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
 
+        for(int i = 1; i <= n; i++){
+            if(djeljivSaSumomSvojihCifara(i))
+                System.out.println(i);
+        }
+
+    }
+
+    private static boolean djeljivSaSumomSvojihCifara(int i) {
+        return sumaCifara(i) % i == 0;
+    }
+
+    private static int sumaCifara(int broj) {
+        int suma = 0;
+        for (int i = 1; i<= broj ; i++)
+            suma+=i;
+        return suma;
     }
 
 
