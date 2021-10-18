8263b96adad2b1b57db447678212b90611dd1af7
iicanovic1 <73068169+iicanovic1@users.noreply.github.com>
2021-10-18 17:13:50 +0200

    initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
